e80fdfdfb4aa8539f4795ee3952d3c91a1a4c9f2
Nicol Yumang <yumangnicol@gmail.com>
2016-04-30 00:39:47 +0800

    Create new users to a project working. SANA

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,32 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        # set_flash_message! :notice, :signed_up
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
   # def after_sign_up_path_for(resource)
   #  'pages/index' # Or :prefix_to_your_route
   # end
 
   def sign_up_params
-    params.require(:staff).permit(:first_name, :last_name, :contract_expiration_date, :email, :password, :password_confirmation)
+    params.require(:staff).permit!
   end
 
   def account_update_params
