5c9b7f3eb700988fad17f45f47c748d035b9671d
demijade <sy_judy@yahoo.com>
2016-05-05 14:58:18 +0800

    admin can see all notifications

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -2,18 +2,24 @@ class UpdatesController < ApplicationController
   
 
   def index
-  	@jobs = Job.where(staff_id: current_staff.id)
-  	id = Array.new
-  	@jobs.each do |job|
-  		project = job.project
-  		b = project.id
-  		id.push(b)
-  	end
-  	@projects = Array.new
-  	id.each do |i|
-  		project = Project.find(i)
-  		@projects.push(project)
-  	end
+    if current_staff.admin == false
+      @jobs = Job.where(staff_id: current_staff.id)
+      id = Array.new
+      @jobs.each do |job|
+        project = job.project
+        b = project.id
+        id.push(b)
+      end
+      @projects = Array.new
+      id.each do |i|
+        project = Project.find(i)
+        @projects.push(project)
+      end
+    else
+      @projects = Project.all
+    end
+
+  	
   	
   	
   	
